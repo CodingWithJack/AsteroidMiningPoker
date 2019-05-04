a886a93b7685f712f019d9c75732706de3eb0e76
CodingWithJack <50020731+CodingWithJack@users.noreply.github.com>
2019-05-04 15:21:17 +0100

    Update and rename cartel.py to cooperation.py

--- Strategies/cooperation.py
+++ Strategies/cooperation.py
@@ -1,8 +1,8 @@
 """
-The cartel strategy.
+The cooperation strategy.
 """
 
-cartel_members = ['MyName', 'Member1', 'Member2']  # The length can be changed
+cooperation_members = ['MyName', 'Member1', 'Member2']  # The length can be changed
 
 
 class Strategy(object):
@@ -29,21 +29,21 @@ class Strategy(object):
 
 class StudentStrategy(Strategy):
     """
-    The cartel will succeed.
+    The cooperation will succeed.
     """
 
     def bid(self, private, public):
         launch = False
         bid = 1
-        players, cartel = [], []
+        players, cooperation = [], []
         for i in public['players']:
             if public['players'][i]['bankroll'] > 0:
                 players.append(i)
-                if i in cartel_members:
-                    cartel.append(i)
-        cartel.sort()
-        if len(players) > len(cartel):
-            if public['round'] % len(cartel) == cartel.index(private['name']):
+                if i in cooperation_members:
+                    cooperation.append(i)
+        cooperation.sort()
+        if len(players) > len(cooperation):
+            if public['round'] % len(cooperation) == cooperation.index(private['name']):
                 launch = True
         else:
             bid = 0
